a73b7da4f0849b8ff4e46efd74f3e8b08ba2f246
gearnode <bryan@frimin.fr>
2020-10-27 18:12:35 +0100

    remove unused base encode and add base64 without padding support

--- src/jose_base.erl
+++ src/jose_base.erl
@@ -14,56 +14,38 @@
 
 -module(jose_base).
 
--export([encode64/1, encode64url/1, encode16/1]).
-
--spec encode64(Bin) -> Base64 when
-      Bin :: binary(),
-      Base64 :: binary().
-encode64(Bin) ->
-    encode64(Bin, <<>>).
-
--spec encode64(Bin, Base64) -> Base64 when
-      Bin :: binary(),
-      Base64 :: binary().
-encode64(<<>>, Acc) ->
-    Acc;
-encode64(<<A:6, B:6, C:6, D:6, Rest/binary>>, Acc) ->
-    encode64(Rest, <<Acc/binary, (enc64(A)), (enc64(B)), (enc64(C)), (enc64(D))>>);
-encode64(<<A:6, B:2>>, Acc) ->
-    <<Acc/binary, (enc64(A)), (enc64(B bsl 4)), $=, $=>>;
-encode64(<<A:6, B:6, C:4>>, Acc) ->
-    <<Acc/binary, (enc64(A)), (enc64(B)), (enc64(C bsl 2)), $=>>.
-
-
--spec enc64(0..63) -> byte().
-enc64(Char) when Char =< 25 ->
-    Char + $A;
-enc64(Char) when Char =< 51 ->
-    Char + $a - 26;
-enc64(Char) when Char =< 61 ->
-    Char + $0 - 52;
-enc64(62) ->
-    $+;
-enc64(63) ->
-    $/.
+-export([encode64url/1, encode64url/2]).
 
 -spec encode64url(Bin) -> Base64URL when
       Bin :: binary(),
       Base64URL :: binary().
 encode64url(Bin) ->
-    encode64url(Bin, <<>>).
+    encode64url(Bin, true, <<>>).
 
--spec encode64url(Bin, Base64URL) -> Base64URL when
+-spec encode64url(Bin, Opts) -> Base64URL when
       Bin :: binary(),
+      Opts :: map(),
       Base64URL :: binary().
-encode64url(<<>>, Acc) ->
+encode64url(Bin, Opts) ->
+    Padding = maps:get(padding, Opts, true),
+    encode64url(Bin, Padding, <<>>).
+
+-spec encode64url(Bin, WithPadding, Base64URL) -> Base64URL when
+      Bin :: binary(),
+      WithPadding :: boolean(),
+      Base64URL :: binary().
+encode64url(<<>>, _Padding, Acc) ->
     Acc;
-encode64url(<<A:6, B:6, C:6, D:6, Rest/binary>>, Acc) ->
-    encode64url(Rest, <<Acc/binary, (enc64url(A)), (enc64url(B)), (enc64url(C)), (enc64url(D))>>);
-encode64url(<<A:6, B:2>>, Acc) ->
+encode64url(<<A:6, B:6, C:6, D:6, Rest/binary>>, Padding, Acc) ->
+    encode64url(Rest, Padding, <<Acc/binary, (enc64url(A)), (enc64url(B)), (enc64url(C)), (enc64url(D))>>);
+encode64url(<<A:6, B:2>>, true, Acc) ->
     <<Acc/binary, (enc64url(A)), (enc64url(B bsl 4)), $=, $=>>;
-encode64url(<<A:6, B:6, C:4>>, Acc) ->
-    <<Acc/binary, (enc64url(A)), (enc64url(B)), (enc64url(C bsl 2)), $=>>.
+encode64url(<<A:6, B:6, C:4>>, true, Acc) ->
+    <<Acc/binary, (enc64url(A)), (enc64url(B)), (enc64url(C bsl 2)), $=>>;
+encode64url(<<A:6, B:2>>, false, Acc) ->
+    <<Acc/binary, (enc64url(A)), (enc64url(B bsl 4))>>;
+encode64url(<<A:6, B:6, C:4>>, false, Acc) ->
+    <<Acc/binary, (enc64url(A)), (enc64url(B)), (enc64url(C bsl 2))>>.
 
 -spec enc64url(0..63) -> byte().
 enc64url(Char) when Char =< 25 ->
@@ -76,23 +58,3 @@ enc64url(62) ->
     $-;
 enc64url(63) ->
     $_.
-
--spec encode16(Bin) -> Base16 when
-      Bin :: binary(),
-      Base16 :: binary().
-encode16(Bin) ->
-    encode16(Bin, <<>>).
-
--spec encode16(Bin, Base16) -> Base16 when
-      Bin :: binary(),
-      Base16 :: binary().
-encode16(<<>>, Acc) ->
-    Acc;
-encode16(<<A:4, B:4, Rest/binary>>, Acc) ->
-    encode16(Rest, <<Acc/binary, (enc16(A)), (enc16(B))>>).
-
--spec enc16(0..15) -> byte().
-enc16(Char) when Char =< 9 ->
-    Char + $0;
-enc16(Char) when Char =< 15 ->
-    Char + $A - 10.
--- test/jose_base_test.erl
+++ test/jose_base_test.erl
@@ -16,15 +16,6 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-encode64_test_() ->
-    [?_assertEqual(<<>>, jose_base:encode64(<<>>)),
-     ?_assertEqual(<<"Zg==">>, jose_base:encode64(<<"f">>)),
-     ?_assertEqual(<<"Zm8=">>, jose_base:encode64(<<"fo">>)),
-     ?_assertEqual(<<"Zm9v">>, jose_base:encode64(<<"foo">>)),
-     ?_assertEqual(<<"Zm9vYg==">>, jose_base:encode64(<<"foob">>)),
-     ?_assertEqual(<<"Zm9vYmE=">>, jose_base:encode64(<<"fooba">>)),
-     ?_assertEqual(<<"Zm9vYmFy">>, jose_base:encode64(<<"foobar">>))].
-
 encode64url_test_() ->
     [?_assertEqual(<<>>, jose_base:encode64url(<<>>)),
      ?_assertEqual(<<"Zg==">>, jose_base:encode64url(<<"f">>)),
@@ -34,11 +25,11 @@ encode64url_test_() ->
      ?_assertEqual(<<"Zm9vYmE=">>, jose_base:encode64url(<<"fooba">>)),
      ?_assertEqual(<<"Zm9vYmFy">>, jose_base:encode64url(<<"foobar">>))].
 
-encode16_test_() ->
-    [?_assertEqual(<<>>, jose_base:encode16(<<>>)),
-     ?_assertEqual(<<"66">>, jose_base:encode16(<<"f">>)),
-     ?_assertEqual(<<"666F">>, jose_base:encode16(<<"fo">>)),
-     ?_assertEqual(<<"666F6F">>, jose_base:encode16(<<"foo">>)),
-     ?_assertEqual(<<"666F6F62">>, jose_base:encode16(<<"foob">>)),
-     ?_assertEqual(<<"666F6F6261">>, jose_base:encode16(<<"fooba">>)),
-     ?_assertEqual(<<"666F6F626172">>, jose_base:encode16(<<"foobar">>))].
+encode64url_without_padding_test_() ->
+    [?_assertEqual(<<>>, jose_base:encode64url(<<>>, #{padding => false})),
+     ?_assertEqual(<<"Zg">>, jose_base:encode64url(<<"f">>, #{padding => false})),
+     ?_assertEqual(<<"Zm8">>, jose_base:encode64url(<<"fo">>, #{padding => false})),
+     ?_assertEqual(<<"Zm9v">>, jose_base:encode64url(<<"foo">>, #{padding => false})),
+     ?_assertEqual(<<"Zm9vYg">>, jose_base:encode64url(<<"foob">>, #{padding => false})),
+     ?_assertEqual(<<"Zm9vYmE">>, jose_base:encode64url(<<"fooba">>, #{padding => false})),
+     ?_assertEqual(<<"Zm9vYmFy">>, jose_base:encode64url(<<"foobar">>, #{padding => false}))].
