431962ce1aed4cb220c37a23b943cf6a74235f33
gearnode <bryan@frimin.fr>
2020-11-06 15:53:51 +0100

    remove commented code

--- src/jose.erl
+++ src/jose.erl
@@ -14,8 +14,6 @@
 
 -module(jose).
 
-%% -export([serialize_header/1]).
-
 -export_type([header/0,
               header_parameter_name/0,
               kid/0,
@@ -40,23 +38,3 @@
 -type certificate_thumbprint() :: binary().
 -type typ() :: binary().
 -type cty() :: binary().
-
-%% -spec serialize_header(header()) -> binary().
-%% serialize_header(Header) ->
-%%     DumpFun = fun(K, V, Acc) -> maps:put(K, serialize_header_parameter_name(K, V), Acc) end,
-%%     Header2 = maps:fold(DumpFun, #{}, Header),
-%%     Header3 = json:serialize(Header2, #{return_binary => true}),
-%%     jose_base:encode64url(Header3).
-
-%% -spec serialize_header_parameter_name(atom(), term()) -> json:value().
-%% serialize_header_parameter_name(jku, Value) ->
-%%     uri:serialize(Value);
-%% serialize_header_parameter_name(x5u, Value) ->
-%%     uri:serialize(Value);
-%% serialize_header_parameter_name(x5c, Value) ->
-%%     list:map(fun(Cert) ->
-%%                      EncodedCert = public_key:der_encode(<<"TODO">>, Cert),
-%%                      jose_base:encode64url(EncodedCert)
-%%              end, Value);
-%% serialize_header_parameter_name(_Key, Value) ->
-%%     Value.
