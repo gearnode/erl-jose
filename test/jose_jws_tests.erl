7379b449146fa5da9c889a553a03313d1d4a0c6a
gearnode <bryan@frimin.fr>
2020-12-11 12:29:36 +0100

    add test for duplicated json key

--- test/jose_jws_tests.erl
+++ test/jose_jws_tests.erl
@@ -73,6 +73,7 @@ decode_compact_test_() ->
      fun decode_jws_with_invalid_cty_header/0,
      fun decode_jws_with_invalid_crit_header/0,
      fun decode_jws_with_invalid_b64_header/0,
+     fun decode_jws_with_header_duplicated_json_key/0,
      fun decode_jws_with_invalid_payload/0,
      fun decode_jws_with_invalid_signature/0,
      fun decode_jws_with_invalid_non_base64_payload/0,
@@ -180,6 +181,10 @@ decode_jws_with_invalid_crit_header() ->
 decode_jws_with_invalid_b64_header() ->
     ?assertMatch({error, {invalid_header, b64, invalid_format}},
                  jose_jws:decode_compact(<<"eyJhbGciOiJIUzI1NiIsImI2NCI6ImZvbyJ9.e30.">>, none, <<>>)).
+
+decode_jws_with_header_duplicated_json_key() ->
+    ?assertEqual({error, {invalid_header, #{position => {1,16}, reason => {duplicate_key, <<"alg">>}}}},
+                 jose_jws:decode_compact(<<"eyJhbGciOiJIUzI1NiIsImFsZyI6Im5vbmUifQ.e30.">>, none, <<>>)).
 
 decode_jws_with_invalid_payload() ->
     ?assertMatch({error, {invalid_payload, {invalid_base64_char, _}}},
