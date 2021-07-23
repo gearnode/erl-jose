81c37f7abb57395ea63b1116273c6819256b3026
gearnode <bryan@frimin.fr>
2021-07-23 18:16:39 +0200

    Add jwk set from_map/1

--- src/jose_jwk_set.erl
+++ src/jose_jwk_set.erl
@@ -14,7 +14,7 @@
 
 -module(jose_jwk_set).
 
--export([from_file/1]).
+-export([from_file/1, from_map/1]).
 
 -export_type([set/0]).
 
@@ -25,29 +25,33 @@ from_file(Filename) ->
   case file:read_file(Filename) of
     {ok, File} ->
       case json:parse(File) of
-        {ok, #{<<"keys">> := Keys}} when is_list(Keys) ->
-          F = fun
-                (Key) when is_map(Key) ->
-                  case jose_jwk:decode(Key) of
-                    {ok, JWK} ->
-                      JWK;
-                    {error, Reason} ->
-                      throw({error, Reason})
-                  end;
-                (_) ->
-                  throw({error, invalid_format})
-              end,
-          try
-            lists:reverse(lists:map(F, Keys))
-          catch
-            throw:{error, Reason} ->
-              {error, Reason}
-          end;
-        {ok, _} ->
-          {error, invalid_format};
+        {ok, Data} ->
+          from_map(Data);
         {error, Reason} ->
           {error, {invalid_format, Reason}}
       end;
     {error, Reason} ->
       {error, {invalid_file, Reason}}
   end.
+
+-spec from_map(map()) -> {ok, set()} | {error, term()}.
+from_map(#{<<"keys">> := Keys}) when is_list(Keys) ->
+  F = fun
+        (Key) when is_map(Key) ->
+          case jose_jwk:decode(Key) of
+            {ok, JWK} ->
+              JWK;
+            {error, Reason} ->
+              throw({error, Reason})
+          end;
+        (_) ->
+          throw({error, invalid_format})
+      end,
+  try
+    lists:reverse(lists:map(F, Keys))
+  catch
+    throw:{error, Reason} ->
+      {error, Reason}
+  end;
+from_map(_) ->
+  {error, invalid_format}.
